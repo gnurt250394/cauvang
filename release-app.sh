3e68fe2cfde3855864b71408ffb2520028bef42f
hoang trung <trung.hv@isofhcare.com>
2019-11-06 09:32:30 +0700

    b

--- release-app.sh
+++ release-app.sh
@@ -1,4 +1,4 @@
-echo deploy to ISOFH-QMS-CDHA-ANDROID-TEST
+echo deploy to APP-HACKATHON-ANDROID
 appcenter codepush release-react -a gnurt250394-gmail.com/APP-HACKATHON-ANDROID -d Production
-echo deploy to ISOFH-QMS-CDHA-IOS-TEST
-appcenter codepush release-react -a gnurt250394-gmail.com/APP-HACKATHON-IOS -d Production
+# echo deploy to ISOFH-QMS-CDHA-IOS-TEST
+# appcenter codepush release-react -a gnurt250394-gmail.com/APP-HACKATHON-IOS -d Production
